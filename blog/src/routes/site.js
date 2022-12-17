b844cd6322b997b063fd582c038d7c1e1d3e2804
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2022-12-17 10:13:38 +0700

    add data

--- blog/src/routes/site.js
+++ blog/src/routes/site.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const sitesController = require('../app/controllers/SitesController');
+const timeOut = require('connect-timeout')
+
 
 router.get('/search', sitesController.search);
-router.get('/', sitesController.index);
+router.get('/', , timeOut('10s'), sitesController.index);
 
 module.exports = router;
