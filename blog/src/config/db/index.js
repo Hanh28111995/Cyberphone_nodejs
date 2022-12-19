6bef0fdc1dc05a0372072cf02352f60c00ec9d9f
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2022-12-19 13:50:22 +0700

    add data

--- blog/src/config/db/index.js
+++ blog/src/config/db/index.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 async function connect() {
- try {
-    await mongoose.connect(process.env.MONGO_DB);
+  try {
+    await mongoose.connect(process.env.MONGO_DB, {
+      useUnifiedTopology: true, // For Mongoose 5 only. Remove for Mongoose 6+
+      serverSelectionTimeoutMS: 1000, // Defaults to 30000 (30 seconds)
+    })
+
     console.log('connect successfully')
- }
- catch(error){
+  } catch (error) {
     console.log('connect fail')
- }
+  }
 }
 module.exports = { connect }
